b5905772dd4eff3a4e670749cd18916282dc72c4
AJAY627 <ajay41527@gmail.com>
2021-08-03 13:11:26 +0530

    Create the Cart Component

---
+++ src/Cart.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Cartitem from "./CartItem";
+
+
+class Cart extends React.Component {
+    render() {
+        return (
+        <div className="cart">
+          <Cartitem/>
+          <Cartitem/>
+          <Cartitem/>
+
+        </div>
+        );
+    }
+}
+
+export default Cart; 
